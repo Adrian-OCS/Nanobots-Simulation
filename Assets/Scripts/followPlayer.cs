e7532ab4c7d19a0e48e86f97b6b22ef6117046b1
Adrian-OCS <adrian.cruz4@upr.edu>
2022-08-16 16:43:17 -0400

    Edit followPlayer script comments

--- Assets/Scripts/followPlayer.cs
+++ Assets/Scripts/followPlayer.cs
@@ -12,7 +12,6 @@ public class followPlayer : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
