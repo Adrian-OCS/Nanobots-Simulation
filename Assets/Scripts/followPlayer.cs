5603f0ef11cf45040264e20d9c826aebab2fbe45
Adrian-OCS <adrian.cruz4@upr.edu>
2022-08-16 16:33:18 -0400

    Implemented tumor collision ability

--- Assets/Scripts/followPlayer.cs
+++ Assets/Scripts/followPlayer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class followPlayer : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
 
